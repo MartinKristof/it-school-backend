f8116036bfa50ba06f113c59acdbb0fffccd0c12
Martin Kristof <Martin.Kristof@lmc.eu>
2019-03-24 21:57:03 +0100

    Add course entity and fix migrations

--- src/Migrations/Version20190324204701.php
+++ src/Migrations/Version20190324204701.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190324200416 extends AbstractMigration
+final class Version20190324204701 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20190324200416 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, urlpath LONGTEXT NOT NULL, title VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, price DOUBLE PRECISION NOT NULL, addresses LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', duration DOUBLE PRECISION NOT NULL, images LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', content LONGTEXT NOT NULL, ratings LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', tags LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', summary LONGTEXT NOT NULL COMMENT \'(DC2Type:object)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20190324200416 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE course');
     }
 }
